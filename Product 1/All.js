7ae9988db60fe810702cd6d9d33d66bf4d6508af
bear80416 <67623360+bear80416@users.noreply.github.com>
2020-08-18 14:17:15 +0800

    up

--- Product 1/All.js
+++ Product 1/All.js
@@ -1,22 +1,35 @@
-
-        $(document).ready(function () {
-            $(".owl-carousel").owlCarousel();
-        });
-
-        /* $('.owl-carousel').owlCarousel({
-            loop: true,
-            
-            margin: 0,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 5
-                }
+//幻燈片
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
             }
-        }) */
+        }
+    })
+});
+
+
+//卷軸向下滑動，NAV自動收起來
+
+var bodyClass = document.body.classList,
+lastScrollY = 0;
+window.addEventListener('scroll', function () {
+    var st = this.scrollY;
+    if (st < lastScrollY) {
+        bodyClass.remove('hideUp');
+    } else {
+        bodyClass.add('hideUp');
+    }
+    lastScrollY = st;
+
+})
